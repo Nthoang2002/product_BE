7df3f1f9066d7cb782d9dd1a630570a04f1df35d
Nthoang2002 <123498505+Nthoang2002@users.noreply.github.com>
2024-05-13 09:52:34 +0700

    Xây dựng phần quản lý danh mục sản phẩm (Phần 2)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,5 +1,8 @@
 const ProductCategory = require("../../models/product-category.model");
+
 const systemConfig = require("../../config/system.js")
+
+const createTreeHelper = require("../../helpers/createTree.js")
 
 // [GET] /admin/product-category
 module.exports.index = async (req, res) => {
@@ -7,18 +10,33 @@ module.exports.index = async (req, res) => {
         deleted: false,
     }
 
+
     const records = await ProductCategory.find(find);
     
+    const newRecords = createTreeHelper.tree(records);
+
     res.render("admin/pages/products-category/index", {
         pageTitle: "Danh mục sản phẩm",
-        records: records
+        records: newRecords
     });
 }
 
 // [GET] /admin/product-category/create
 module.exports.create = async (req, res) => {
+    let find = {
+        deleted: false,
+    }
+
+
+    const records = await ProductCategory.find(find);
+
+    const newRecords = createTreeHelper.tree(records);
+    console.log((newRecords))
+
+
     res.render("admin/pages/products-category/create", {
         pageTitle: "Tạo danh mục sản phẩm",
+        records: records
     });
 }
 
